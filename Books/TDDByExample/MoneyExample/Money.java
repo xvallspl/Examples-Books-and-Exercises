278b760b7f7eb24830276b0bd4f9a181571d2a87
Xavier Valls <xaviervallspla@gmail.com>
2014-09-03 06:59:44 +0200

    End of TDD by Example: Part I, chapter 8: Makin' Objects.

--- Books/TDDByExample/MoneyExample/Money.java
+++ Books/TDDByExample/MoneyExample/Money.java
@@ -1,7 +1,18 @@
 package Money;
 
-public class Money {
+abstract class Money {
+	
 	protected int amount;
+	
+	abstract Money times(int multiplier);
+	
+	static Money dollar(int amount){
+		return new Dollar(amount);
+	}
+	
+	static Money franc(int amount){
+		return new Franc(amount);
+	}
 	
 	public boolean equals(Object object){
 		Money money = (Money) object;
