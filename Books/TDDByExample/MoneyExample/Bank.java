69173cac7ecedbe7c1549ae9232ddd1795c3976b
Xavier Valls <xaviervallspla@gmail.com>
2014-09-09 18:01:22 +0200

    End of TDD by Example: Part I, chapter 14: Change.

--- Books/TDDByExample/MoneyExample/Bank.java
+++ Books/TDDByExample/MoneyExample/Bank.java
@@ -1,13 +1,26 @@
 package money;
 
+import java.util.Hashtable;
+
 public class Bank {
 
+	private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
 	Bank(){
 		
 	}
 	
 	Money reduce(Expression source, String to) {
-		return source.reduce(to);
+		return source.reduce(this, to);
+	}
+	
+	int rate(String from, String to){
+		if (from.equals(to)) return 1;
+		Integer rate = (Integer) rates.get(new Pair(from,to));
+		return rate.intValue();
+	}
+
+	 void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from,to), new Integer(rate));
 	}
 	
 }
--- Books/TDDByExample/MoneyExample/MoneyTest.java
+++ Books/TDDByExample/MoneyExample/MoneyTest.java
@@ -65,5 +65,18 @@ public class MoneyTest {
     	Money result = bank.reduce(Money.dollar(1), "USD");
     	assertEquals(result, Money.dollar(1));
     }
+    
+    @Test
+    public void testReduceMoneyDifferentCurrency(){
+    	Bank bank = new Bank();
+    	bank.addRate("CHF", "USD", 2);
+    	Money result = bank.reduce(Money.franc(2), "USD");
+    	assertEquals(result, Money.dollar(1));
+    }
+    
+    @Test
+    public void testIdentityRate(){
+    	assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 }
 
