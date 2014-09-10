da0131c20484c471ba070c15379429c06d46e212
Xavier Valls <xaviervallspla@gmail.com>
2014-09-10 20:48:37 +0200

    End of TDD by Example: Part I, chapter 15: Mixed Currencies.

--- Books/TDDByExample/MoneyExample/Money.java
+++ Books/TDDByExample/MoneyExample/Money.java
@@ -35,11 +35,11 @@ class Money implements Expression{
 	}
 	
 
-	Money times(int multiplier){
+	Expression times(int multiplier){
 		return  new Money(amount*multiplier, currency);
 	}
 	
-	Expression plus(Money addend){
+	public Expression plus(Expression addend){
 		return new Sum(this, addend);
 	}
 	
