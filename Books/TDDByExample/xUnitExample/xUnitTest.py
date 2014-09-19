d0ab14feea06dcad1e37b7cff22ec7e41bed6d5e
Xavier Valls <xaviervallspla@gmail.com>
2014-09-19 07:53:55 +0200

    End of TDD by Example: Part II, chapter 5: Dealing With Failure.

--- Books/TDDByExample/xUnitExample/xUnitTest.py
+++ Books/TDDByExample/xUnitExample/xUnitTest.py
@@ -11,8 +11,11 @@ class TestCase:
 		result = TestResult()
 		result.testStarted()
 		self.setUp()
-		method = getattr(self, self.name)
-		method()
+		try:
+			method = getattr(self, self.name)
+			method()
+		except:
+			result.testFailed()
 		self.tearDown()
 		return result
 
@@ -38,13 +41,17 @@ class WasRun(TestCase):
 class TestResult:
 
 	def __init__(self):
-		self.runCount= 0
+		self.runCount = 0
+		self.errorCount = 0
 
 	def testStarted(self):
 		self.runCount += 1
 
+	def testFailed(self):
+		self.errorCount += 1
+
 	def summary(self):
-		return "%d run, 0 failed" % self.runCount
+		return "%d run, %d failed" %(self.runCount, self.errorCount)
 
 class TestCaseTest(TestCase):
 
@@ -66,7 +73,14 @@ class TestCaseTest(TestCase):
 		result = test.run()
 		assert("1 run, 1 failed"==result.summary())
 
+	def testFailedResultFormatting(self):
+		result = TestResult()
+		result.testStarted()
+		result.testFailed()
+		assert("1 run, 1 failed"==result.summary())
+
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
-#TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
