c3d2ac7a68452439dfe6896b0f1339a1aa21f50c
Xavier Valls <xaviervallspla@gmail.com>
2014-09-20 10:31:29 +0200

    End of TDD by Example: Part II, chapter 6 (Final): How Suite It Is.

--- Books/TDDByExample/xUnitExample/xUnitTest.py
+++ Books/TDDByExample/xUnitExample/xUnitTest.py
@@ -7,8 +7,10 @@ class TestCase:
 	def setUp(self):
 		pass	
 
-	def run(self):
-		result = TestResult()
+	def tearDown(self):
+		pass
+
+	def run(self, result):
 		result.testStarted()
 		self.setUp()
 		try:
@@ -17,7 +19,6 @@ class TestCase:
 		except:
 			result.testFailed()
 		self.tearDown()
-		return result
 
 class WasRun(TestCase):
 
@@ -53,34 +54,56 @@ class TestResult:
 	def summary(self):
 		return "%d run, %d failed" %(self.runCount, self.errorCount)
 
+class TestSuite:
+
+	def __init__(self):
+		self.tests = []
+
+	def add(self, test):
+		self.tests.append(test)
+
+	def run(self, result):
+		for test in self.tests:
+			test.run(result)
+
 class TestCaseTest(TestCase):
+
+	def setUp(self):
+		self.result = TestResult()
 
 	def testTemplateMethod(self):
 		test = WasRun("testMethod")
-		test.run()
+		test.run(self.result)
 		assert("setUp testMethod tearDown " == test.log)
 
-	def tearDown(self):
-		pass
-
 	def testResult(self):
-		test   = WasRun("testMethod")
-		result = test.run()
-		assert("1 run, 0 failed"==result.summary())
+		test = WasRun("testMethod")
+		test.run(self.result)
+		assert("1 run, 0 failed" == self.result.summary())
 
 	def testFailedResult(self):
-		test   = WasRun("testBrokenMethod")
-		result = test.run()
-		assert("1 run, 1 failed"==result.summary())
+		test = WasRun("testBrokenMethod")
+		test.run(self.result)
+		assert("1 run, 1 failed" == self.result.summary())
 
 	def testFailedResultFormatting(self):
-		result = TestResult()
-		result.testStarted()
-		result.testFailed()
-		assert("1 run, 1 failed"==result.summary())
+		self.result.testStarted()
+		self.result.testFailed()
+		assert("1 run, 1 failed" == self.result.summary())
 
+	def testSuite(self):
+		suite = TestSuite()
+		suite.add(WasRun("testMethod"))
+		suite.add(WasRun("testBrokenMethod"))
+		suite.run(self.result)
+		assert("2 run, 1 failed" == self.result.summary())
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
-TestCaseTest("testFailedResultFormatting").run()
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print result.summary()
