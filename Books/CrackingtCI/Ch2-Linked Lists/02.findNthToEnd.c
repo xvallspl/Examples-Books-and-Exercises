433e92d790dc9ae950a68603f5c23d02369364aa
Xavier Valls <xaviervallspla@gmail.com>
2014-09-28 18:55:50 +0200

    CH2:02, coded book's solution

--- Books/CrackingtCI/Ch2-Linked Lists/02.findNthToEnd.c
+++ Books/CrackingtCI/Ch2-Linked Lists/02.findNthToEnd.c
@@ -21,6 +21,32 @@ struct node *myFindNToEnd(struct node *first, int n){
 	return aux2;
 }
 
+struct node *theirFindNToEnd(struct node *first, int n){
+	
+	if(first == NULL || n < 1){
+		return NULL;
+	}
+
+	struct node *p1, *p2;
+	int i, count;
+	p1  = first;
+	p2 = first;
+
+	for (i=0;i<n-1; ++i){ // skip n-1 steps ahead
+		if(p2 == NULL){   
+			return NULL;  // not found since list size < n
+		}
+		p2 = p2->next;
+	}
+
+	while(p2->next!=NULL){
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+
+	return p1;
+}
+
 int main(){
 	struct node *first, *start;
 	
@@ -29,6 +55,9 @@ int main(){
 	printList(first);
 	printf("\n");
 	start = myFindNToEnd(first, 5);
+	printList(start);
+	printf("\n");
+	start = theirFindNToEnd(first, 5);
 	printList(start);
 	printf("\n");
 	//deleteList(first);
