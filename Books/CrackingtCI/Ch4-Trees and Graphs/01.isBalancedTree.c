27107b31f1a8dcd51129f2d9138191fd12d268cc
Xavier Valls <xaviervallspla@gmail.com>
2014-10-07 17:35:55 +0200

    implementation on paper of how to set a binary tree (failed/broken)

---
+++ Books/CrackingtCI/Ch4-Trees and Graphs/01.isBalancedTree.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int cNode;
+
+struct node{
+	int data;
+	struct node *right;
+	struct node *left;
+};
+
+
+int isBalanced(struct node *tree){
+	return 0;
+	//return 1;
+}
+
+void setTree(struct node **tree, int n){
+	if(n>=0){
+		(*tree)->data = n--;
+		printf("%d\n", n);
+		(*tree)->right = (struct node *) malloc(sizeof(struct node));
+		(*tree)->left  = (struct node *) malloc(sizeof(struct node));
+		setTree( &(*tree)->right, n/2);
+		setTree(&(*tree)->left, n/2);
+	}
+}
+
+int main(){
+
+	struct node *tree1,*tree2,*root1, *root2;
+	int i, n;
+
+	tree1 = (struct node *) malloc(sizeof(struct node));
+	root1= tree1;
+	tree2 = (struct node *) malloc(sizeof(struct node));
+	root2= tree2;
+	
+	n=11;
+	setTree(&tree1, n);
+	setTree(&tree2, n);
+}
