e8c33d1703a592f90eca3b39e6a4b05e6b07f7bf
Xavier Valls <xaviervallspla@gmail.com>
2014-10-23 21:47:50 +0200

    CH4-E07: Check if subtree (version as on paper)

--- Books/CrackingtCI/Ch4-Trees and Graphs/07.isSubtree.c
+++ Books/CrackingtCI/Ch4-Trees and Graphs/07.isSubtree.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 struct node{
 	int data;
 	struct node *right;
 	struct node *left;
 };
-
+//Finds first appearance
 struct node *findNode(struct node *root, int data){
 	if (root == NULL) return NULL;
 	if(root->data!= data){
@@ -16,5 +17,20 @@ struct node *findNode(struct node *root, int data){
 		return (finding->data == data) ? finding: findNode(root->left, data);
 	}
 	return root;
+}
+//Suposing just one appearance
+int isSubtree(struct node *rootBig, struct node *rootSmall){
+	struct node *start;
+	start = findNode( rootBig, rootSmall->data);
+	return checkSubTree(rootBig, start)
+}
+
+int checkSubTree(struct node *rootBig, struct node *rootSmall){
+	if(rootBig==NULL && rootSmall==NULL) return 1;
+	if(rootBig->data != rootSmall->data) return 0;
+	else{
+		return checkSubTree(rootBig->right, rootSmall->right) && 
+			     checkSubTree(rootBig->left, rootSmall->left);
+	}
 }
 
