fd729c3d1d3a7783ac3b3bf13bcdc6693e01b2c3
Xavier Valls <xaviervallspla@gmail.com>
2014-10-24 18:14:56 +0200

    CH4-E7: Added book's solution

--- Books/CrackingtCI/Ch4-Trees and Graphs/07.isSubtree.c
+++ Books/CrackingtCI/Ch4-Trees and Graphs/07.isSubtree.c
@@ -1,3 +1,6 @@
+/*You have two very large binary trees: T1, with millions of nodes, and T2, with hundreds 
+of nodes. Create an algorithm to decide if T2 is a subtree of T1.*/
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -34,3 +37,31 @@ int checkSubTree(struct node *rootBig, struct node *rootSmall){
 	}
 }
 
+int theirContainsTree(struct node *t1, struct node *t2) {
+	if (t2 == NULL) 
+		return 1; // The empty tree is always a subtree
+	else 
+		return theirSubTree(t1, t2);
+}
+int theirSubTree(struct node *r1, struct node *r2) {
+	
+	if (r1 == NULL) return 0; // big tree empty & subtree still not found.
+	if (r1->data == r2->data) {
+		if (theirMatchTree(r1,r2))
+			return 1;
+	}
+	return (theirSubTree(r1->left, r2) || theirSubTree(r1->right, r2));
+}
+
+int theirMatchTree(struct node *r1, struct node *r2) {
+	
+	if (r2 == NULL && r1 == NULL)
+		 return 1; // nothing left in the subtree
+	if (r1 == NULL || r2 == NULL)
+		 return 0; // big tree empty & subtree still not found
+	if (r1->data != r2->data) 
+		return 0; // data doesn’t match
+	return (theirMatchTree(r1->left, r2->left) &&
+			 theirMatchTree(r1->right, r2->right));
+	}
+}
--- Books/CrackingtCI/Ch4-Trees and Graphs/Readme.md
+++ Books/CrackingtCI/Ch4-Trees and Graphs/Readme.md
@@ -4,3 +4,7 @@ CtCI, Chapter 4: Trees and Graphs
 Exercises from the book Cracking the Code Interview. Chapter 3: Trees and Graphs
 
 1. Function to determine if a binary tree is balanced (C)
+3. Create a binary tree with minimal height from an array (C)
+4. Linked list of elements in a level (C)
+5. Find the next in-order successor of a node in a tree with pointers to parent(C)
+7. Find subtree
